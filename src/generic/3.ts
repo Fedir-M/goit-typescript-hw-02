c27df6917a0034953736e3e5066f5bfb490baad3
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2024-03-16 16:36:17 -0400

    hw02, v2.0

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -4,7 +4,7 @@
 */
 
 function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
-  return Object.assign(objA, objB) as T & U; //? а якщо замість (: T & U) написати (: any) наскільки це буде корректно? )))
+  return Object.assign(objA, objB);
 }
 
 export {};
